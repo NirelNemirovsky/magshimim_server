ae9a7d2a33185ff979daf5de91f02c2e8a846190
Nirel Nemirovsky <nemirovsky.nirel@gmail.com>
2018-02-16 23:54:16 +0200

    added speech files

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+// gets the audio file from mobile
+app.post('/audio', (req, res) =>
+
+)
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
