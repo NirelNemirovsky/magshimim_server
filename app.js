177513f951433512749a3c98c2bab95029a7c450
Nirel Nemirovsky <nemirovsky.nirel@gmail.com>
2018-02-17 12:46:01 +0200

    Receiving base64 from client

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var fs = require('fs');
+var speech = require('./speech');
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-// gets the audio file from mobile
-app.post('/audio', (req, res) =>
 
-)
+// gets the audio file from mobile
+app.post('/audio', (req, res) =>{
+  speech.recognize(req.body.data)
+  // console.log(req.body.data);
+  res.sendStatus(200);
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
